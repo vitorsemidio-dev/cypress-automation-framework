bedf881a1f678b1d928d0fc081b02d8f3900dd0b
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-11-13 11:02:38 -0300

    test: add assertion for , ]2;]1; and

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -3,6 +3,9 @@
 describe("Test Contact Us form via WebdriverUni", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
     cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.document().should("have.property", "charset").and("eq", "UTF-8");
+    cy.title().should("include", "WebDriver | Contact Us");
+    cy.url().should("include", "contactus");
     cy.get('[name="first_name"]').type("Jane");
     cy.get('[name="last_name"]').type("Doe");
     cy.get('[name="email"]').type("jane_doe@email.com");
