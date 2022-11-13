5af2efc2af32f0c4e35597ec302258aa7800237a
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-11-13 10:38:06 -0300

    test: add assertion

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -4,11 +4,12 @@
 describe("Test Contact Us form via Automation Test Store", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
     cy.visit("https://www.automationteststore.com/");
-    cy.xpath(`//a[contains(@href, 'contact')]`).click();
+    cy.get("a[href$='contact']").click();
     cy.get("#ContactUsFrm_first_name").type("Jane");
     cy.get("#ContactUsFrm_email").type("jane_doe@email.com");
+    cy.get("#ContactUsFrm_email").should("have.attr", "name", "email");
     cy.get("#ContactUsFrm_enquiry").type("Lorem ipsum dolor, sit amet consectetur adipisicing elit.");
-    cy.get(".col-md-6 > .btn").click();
-    cy.contains("Your enquiry has been successfully sent to the store owner!").should("be.visible");
+    cy.get("button[title='Submit']").click();
+    cy.get(".mb40 > :nth-child(3)").should("have.text", "Your enquiry has been successfully sent to the store owner!");
   });
 });
