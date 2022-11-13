f565da7c4c8080f90436badc3f9ae4e882341230
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-11-13 17:03:45 -0300

    test: WIP calculate total of normal and sale products

--- cypress/e2e/automation-test-store/alias-and-invoke.js
+++ cypress/e2e/automation-test-store/alias-and-invoke.js
@@ -12,9 +12,31 @@ describe("Alias and invoke", () => {
 
   it("Validate product thumbnail in home page", () => {
     cy.visit("https://www.automationteststore.com/");
-    cy.get(".thumbnail").as("productThumbnailList");
-    cy.get("@productThumbnailList").should("have.length", 16);
-    cy.get("@productThumbnailList").find(".productcart");
-    cy.get("@productThumbnailList").find(".productcart").invoke("attr", "title").should("include", "Add to Cart");
+    cy.get(".thumbnail").as("productThumbnail");
+    cy.get("@productThumbnail").should("have.length", 16);
+    cy.get("@productThumbnail").find(".productcart");
+    cy.get("@productThumbnail").find(".productcart").invoke("attr", "title").should("include", "Add to Cart");
+  });
+
+  it.only("Calculate total of normal and sale products", () => {
+    cy.visit("https://www.automationteststore.com/");
+    cy.get(".thumbnail").as("productThumbnail");
+    // cy.get("@productThumbnail")
+    //   .find(".oneprice")
+    //   .each(($el, index, $list) => {
+    //     cy.log($el.text());
+    //   });
+    cy.get(".thumbnail").find(".oneprice").invoke("text").as("itemPrice");
+    let itemsTotal = 0;
+    cy.get("@itemPrice").then(($itemPrice) => {
+      let itemsPriceTotal = 0;
+      const itemsNoSalePrice = $itemPrice.split("$");
+      for (let i = 0; i < itemsNoSalePrice.length; i++) {
+        cy.log(itemsNoSalePrice[i]);
+        itemsPriceTotal += Number(itemsNoSalePrice[i]);
+      }
+      cy.log("Non sale price items total: " + itemsPriceTotal);
+      itemsTotal += itemsPriceTotal;
+    });
   });
 });
