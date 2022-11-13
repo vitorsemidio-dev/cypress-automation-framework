f1585f9f6059f812babcecb47a984c27f4064ae7
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-11-13 11:59:30 -0300

    test: inspect Automation Test Store with `get`, `contains`, `find` and `eq`

---
+++ cypress/e2e/automation-test-store/inspect.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+/// <reference types="cypress-xpath" />
+
+describe("Inspect Automation Test Store items using chain of commands", () => {
+  it("Click on the first item using item header", () => {
+    cy.visit("https://www.automationteststore.com/");
+    cy.get("#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname").click();
+  });
+
+  it("Click on the first item using item text", () => {
+    cy.visit("https://www.automationteststore.com/");
+    cy.get(".prdocutname").contains("Skinsheen Bronzer Stick").click();
+  });
+
+  it("Click on the first item using item index", () => {
+    cy.visit("https://www.automationteststore.com/");
+    cy.get(".fixed_wrapper").find(".prdocutname").eq(0).click();
+  });
+});
