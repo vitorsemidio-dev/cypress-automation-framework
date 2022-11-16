ab6f9cbc05e8ebc2f7ac8f598d1c0eb2cd726290
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-11-16 06:19:25 -0300

    refactor: fill data with 'Cypress.env'

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,13 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,tsx,tsx,feature}",
+    excludeSpecPattern: "cypress/e2e/other/*.js",
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10_000,
+    pageLoadTimeout: 120_000,
   },
+  env: {
+    first_name: "Sarah"
+  }
 });
--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -23,4 +23,17 @@ describe("Test Contact Us form via WebdriverUni", () => {
     cy.get("body").contains("Error: all fields are required");
     cy.get("body").contains("Error: Invalid email address");
   });
+
+  it("Should be able to submit a successful submission via contact us form using 'Cypress.env'", () => {
+    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.document().should("have.property", "charset").and("eq", "UTF-8");
+    cy.title().should("include", "WebDriver | Contact Us");
+    cy.url().should("include", "contactus");
+    cy.get('[name="first_name"]').type(Cypress.env("first_name"));
+    cy.get('[name="last_name"]').type("Doe");
+    cy.get('[name="email"]').type("jane_doe@email.com");
+    cy.get("textarea.feedback-input").type("Lorem ipsum dolor sit amet consectetur, adipisicing elit.");
+    cy.get('[type="submit"]').click();
+    cy.get("h1").should("have.text", "Thank You for your Message!");
+  });
 });
