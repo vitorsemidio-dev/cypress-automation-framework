2fc88a40d5e8d09a95f7c62640d9f117cb5a5a95
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-04-19 22:39:24 -0300

    chore: ci configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "rkd47h",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
